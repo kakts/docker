ed464d554b14951a99a99875aec5b3b4d627eb5e
kakts <kakts.git@gmail.com>
2017-06-04 21:25:26 +0900

    Fix identidock

--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# 環境変数によって分ける
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
   exec python "identidock.py"
 else
   echo "Running Production Server"
-  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
+  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+             --callable app --stats 0.0.0.0:9191
 fi
